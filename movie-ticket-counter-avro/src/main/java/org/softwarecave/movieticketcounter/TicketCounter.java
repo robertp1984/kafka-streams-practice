8c5031b6b45194f42eca2b03bed30b64ca788897
robertp1984 <piasecki.robert@gmail.com>
2026-04-03 19:21:53 +0200

    Added Movie Ticket Counter using Avro format in input and output topics.

---
+++ movie-ticket-counter-avro/src/main/java/org/softwarecave/movieticketcounter/TicketCounter.java
@@ -0,0 +1,121 @@
+package org.softwarecave.movieticketcounter;
+
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.softwarecave.movieticketcounter.avro.TicketSellEvent;
+import org.softwarecave.movieticketcounter.avro.TicketSoldSummary;
+
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
+
+@Slf4j
+public class TicketCounter {
+
+    public static final String INPUT_TOPIC = "ticket-sell-event";
+    public static final String OUTPUT_TOPIC = "ticket-sold-summary";
+
+    public static void main() throws InterruptedException {
+        TicketCounter ticketCounter = new TicketCounter();
+        ticketCounter.run();
+    }
+
+    public void run() throws InterruptedException {
+        var config = createConfig();
+
+        SpecificAvroSerde<TicketSellEvent> ticketSellEventSerde = createAvroSerde(TicketSellEvent.class, config);
+        SpecificAvroSerde<TicketSoldSummary> ticketSoldSummarySerde = createAvroSerde(TicketSoldSummary.class, config);
+        var topology = createTopology(config, ticketSellEventSerde, ticketSoldSummarySerde);
+
+        // intentionally not using try-with-resources here to keep the application running until shutdown
+        KafkaStreams streams = new KafkaStreams(topology, config);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Stop requested. Exiting.");
+            streams.close();
+            latch.countDown();
+        }));
+
+        streams.start();
+        latch.await();
+    }
+
+    Properties createConfig() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "movie-ticket-counter-avro");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE_V2);
+        props.put(SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        return props;
+    }
+
+    Topology createTopology(Properties config, SpecificAvroSerde<TicketSellEvent> ticketSellEventSerde, SpecificAvroSerde<TicketSoldSummary> ticketSoldSummarySerde) {
+        Serde<String> stringSerde = Serdes.String();
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, TicketSellEvent> inputStream = builder.stream(INPUT_TOPIC,
+                Consumed.with(stringSerde, ticketSellEventSerde));
+
+        KTable<String, TicketSoldSummary> aggregatedTable = inputStream
+                .peek((k, v) -> log.info("Input event: {}:{}", k, v))
+                .filter((k, v) -> v.getTicketCount() > 0
+                        && v.getTicketCount() == v.getSeats().size())
+                .selectKey((k, v) -> v.getMovieTitle())
+                .groupByKey()
+                .aggregate(this::createInitialTicketSoldSummary,
+                        (k, v, summary) -> addToTicketSoldSummary(summary, k, v),
+                        Materialized.<String, TicketSoldSummary, KeyValueStore<Bytes, byte[]>>as("aggr")
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(ticketSoldSummarySerde));
+
+        aggregatedTable.toStream().to(OUTPUT_TOPIC, Produced.with(stringSerde, ticketSoldSummarySerde));
+
+        return builder.build();
+    }
+
+    private TicketSoldSummary addToTicketSoldSummary(TicketSoldSummary summary, String k, TicketSellEvent v) {
+        var result = TicketSoldSummary.newBuilder()
+                .setMovieTitle(k)
+                .setTicketsSoldTotal(summary.getTicketsSoldTotal() + v.getTicketCount())
+                .build();
+        return result;
+    }
+
+    private TicketSoldSummary createInitialTicketSoldSummary() {
+        return TicketSoldSummary.newBuilder()
+                .setMovieTitle("")
+                .setTicketsSoldTotal(0L)
+                .build();
+    }
+
+    <T extends SpecificRecord> SpecificAvroSerde<T> createAvroSerde(Class<T> klass, Properties config) {
+        final SpecificAvroSerde<T> serde = new SpecificAvroSerde<>();
+        serde.configure(Collections.singletonMap(
+                SCHEMA_REGISTRY_URL_CONFIG, config.getProperty(SCHEMA_REGISTRY_URL_CONFIG)
+        ), false);
+        return serde;
+    }
+}
+
---
+++ movie-ticket-counter-avro/src/test/java/org/softwarecave/movieticketcounter/TicketCounterTest.java
@@ -0,0 +1,110 @@
+package org.softwarecave.movieticketcounter;
+
+import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TestOutputTopic;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.softwarecave.movieticketcounter.avro.TicketSellEvent;
+import org.softwarecave.movieticketcounter.avro.TicketSoldSummary;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TicketCounterTest {
+
+    private MockSchemaRegistryClient srClient;
+    private SpecificAvroSerde<TicketSellEvent> ticketSellEventSerde;
+    private SpecificAvroSerde<TicketSoldSummary> ticketSoldSummarySerde;
+
+    private TopologyTestDriver testDriver;
+
+    @BeforeEach
+    public void setUp() {
+        TicketCounter ticketCounter = new TicketCounter();
+        var config = ticketCounter.createConfig();
+
+        srClient = new MockSchemaRegistryClient();
+        ticketSellEventSerde = createAvroSerde(TicketSellEvent.class, config, srClient);
+        ticketSoldSummarySerde = createAvroSerde(TicketSoldSummary.class, config, srClient);
+        var topology = ticketCounter.createTopology(config, ticketSellEventSerde, ticketSoldSummarySerde);
+
+        testDriver = new TopologyTestDriver(topology, config);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        testDriver.close();
+    }
+
+    @Test
+    public void testOneEvent() {
+        var inputTopic = createInputTopic();
+        var outputTopic = createOutputTopic();
+
+        inputTopic.pipeInput(null, new TicketSellEvent("Good Movie", 2, List.of("3A", "3B")));
+
+        var outputTopicMap = outputTopic.readKeyValuesToMap();
+        assertThat(outputTopicMap.size()).isEqualTo(1);
+        assertThat(outputTopicMap).containsKey("Good Movie");
+        assertThat(outputTopicMap.get("Good Movie"))
+                .hasFieldOrPropertyWithValue("movieTitle", "Good Movie")
+                .hasFieldOrPropertyWithValue("ticketsSoldTotal", 2L);
+    }
+
+    @Test
+    public void testSixEventsAndThreeMovies() {
+        var inputTopic = createInputTopic();
+        var outputTopic = createOutputTopic();
+
+        inputTopic.pipeInput(null, new TicketSellEvent("Good Movie", 2, List.of("3A", "3B")));
+        inputTopic.pipeInput(null, new TicketSellEvent("Medium Movie", 2, List.of("4F", "4G")));
+        inputTopic.pipeInput(null, new TicketSellEvent("Medium Movie", 1, List.of("5A")));
+        inputTopic.pipeInput(null, new TicketSellEvent("Good Movie", 4, List.of("6A", "6B", "6C", "6D")));
+        inputTopic.pipeInput(null, new TicketSellEvent("Awesome Movie", 1, List.of("7A")));
+        inputTopic.pipeInput(null, new TicketSellEvent("Good Movie", 5, List.of("6A", "6B", "6D", "6E", "6F")));
+
+        var outputTopicMap = outputTopic.readKeyValuesToMap();
+        assertThat(outputTopicMap.size()).isEqualTo(3);
+        assertThat(outputTopicMap).containsKey("Good Movie");
+        assertThat(outputTopicMap.get("Good Movie"))
+                .hasFieldOrPropertyWithValue("movieTitle", "Good Movie")
+                .hasFieldOrPropertyWithValue("ticketsSoldTotal", 11L);
+        assertThat(outputTopicMap.get("Medium Movie"))
+                .hasFieldOrPropertyWithValue("movieTitle", "Medium Movie")
+                .hasFieldOrPropertyWithValue("ticketsSoldTotal", 3L);
+        assertThat(outputTopicMap.get("Awesome Movie"))
+                .hasFieldOrPropertyWithValue("movieTitle", "Awesome Movie")
+                .hasFieldOrPropertyWithValue("ticketsSoldTotal", 1L);
+    }
+
+    private TestInputTopic<String, TicketSellEvent> createInputTopic() {
+        return testDriver.createInputTopic(TicketCounter.INPUT_TOPIC, new StringSerializer(),
+                ticketSellEventSerde.serializer());
+    }
+
+    private TestOutputTopic<String, TicketSoldSummary> createOutputTopic() {
+        return testDriver.createOutputTopic(TicketCounter.OUTPUT_TOPIC, new StringDeserializer(),
+                ticketSoldSummarySerde.deserializer());
+    }
+
+    <T extends SpecificRecord> SpecificAvroSerde<T> createAvroSerde(Class<T> klass, Properties config,
+                                                                    SchemaRegistryClient srClient) {
+        final SpecificAvroSerde<T> serde = new SpecificAvroSerde<>(srClient);
+        serde.configure(Collections.singletonMap(
+                SCHEMA_REGISTRY_URL_CONFIG, config.getProperty(SCHEMA_REGISTRY_URL_CONFIG)
+        ), false);
+        return serde;
+    }
+}
